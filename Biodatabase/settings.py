451b6722f25fc7d77ecc0937422d2379ab27ff06
ryanunique <delacruzryan63@gmail.com>
2023-11-13 11:40:20 +0800

    changes

--- Biodatabase/settings.py
+++ Biodatabase/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i0_mz4f5&@x%qun9$aa++jx9snwd2k(7ceo#465c5qvr$m+6i*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
